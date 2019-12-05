fb477a1101a8fbd99c0fa259cac8c807621fc7fb
fanyucai1 <fanyucai1@126.com>
2019-12-05 15:12:49 +0800

    no

---
+++ CMDB.py
@@ -0,0 +1,46 @@
+import requests
+import demjson
+import os
+from pathos.pools import ProcessPool
+key=''######you want to input
+################################################
+if not os.path.exists("CMDB.tsv"):
+    outfile = open("CMDB.tsv", "w")
+    outfile.write("#Chr\tPos\tID\tRef\tAlt\tallele_count\tallele_freq\n")
+    outfile.close()
+def run(chr,pos):
+    html = 'http://db.cngb.org/cmdb/api/v1.0/variant?type=position&query=%s-%s&token=%s'%(chr,pos,key)
+    try:
+        #res = requests.get(html,proxies={"http":"http://61.145.8.172:9999"})
+        res = requests.get(html)
+        ret = demjson.decode(res.text)
+        print(ret)
+        outfile = open("CMDB.tsv", "a+")
+        for entry in ret:
+            if not 'code' in entry:
+                outfile.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n"%(entry['chrom'],entry['pos'],entry['rsid'],entry['ref'],entry['alt'],entry['allele_count'],entry['allele_freq']))
+        outfile.close()
+    except:
+        pass
+if __name__=="__main__":
+    infile = open("tmp.bed", "r")
+    infile2=open("CMDB.tsv", "r")
+    site={}
+    for line in infile2:
+        if not line.startswith("#"):
+            line=line.strip()
+            array=line.split("\t")
+            site[array[0]]=1
+    infile2.close()
+    for line in infile:
+        if not line.startswith("#"):
+            line = line.strip()
+            array = line.split("\t")
+            pos = []
+            chr = []
+            for i in range(int(array[1]), int(array[2]) + 1):
+                if i not in site:
+                    pos.append(i)
+                    chr.append(array[0])
+            pool = ProcessPool(nodes=3)
+            pool.map(run, chr, pos)
---
+++ README.md
@@ -0,0 +1,7 @@
+中国人群频率数据库：https://db.cngb.org/cmdb/
+
+注释分析工具：https://github.com/ShujiaHuang/cmdbtools
+
+中国人群频率数据库发的文章在线解读：https://cloud.tencent.com/developer/news/326330
+
+Siyang Liu, Shujia Huang. et al.(2018) Genomic Analyses from Non-invasive Prenatal Testing Reveal Genetic Associations, Patterns of Viral Infections, and Chinese Population History. Cell, 2, 347-359.DOI:https://doi.org/10.1016/j.cell.2018.08.016
